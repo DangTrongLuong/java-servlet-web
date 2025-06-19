adba30e322b3150c01bf2ca63a0a3faf72403589
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-06-19 16:35:13 +0700

    up code

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World, I am a servlet,chú ý nhánh của dangluong-17.06.2025!</h1>");
+        out.println("<p>Neu ban thay cai nya nghia la ban da chay duoc</p>");
+    }
+}
